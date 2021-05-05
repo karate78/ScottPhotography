21c64ebce51c8d5f23b8105b390f415567bbb25d
karate78 <karatephotography@gmail.com>
2021-05-04 21:30:54 -0500

    initial commit

---
+++ ScottPhotography.UI.MVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ScottPhotography.UI.MVC.Startup))]
+namespace ScottPhotography.UI.MVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
